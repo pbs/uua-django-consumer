b4c31180c7eef2a3cd49b6a4d92b457156730e2f
Ion Scerbatiuc <ion.scerbatiuc@lpro.leverpointinc.com>
2010-04-22 16:14:38 +0300

    Refacored render_failure to a more general case (even success). Modified the template to account for this. Updated the slidingmessage jquery plugin to provide a callback for sliding-out.

--- src/pbs_uua_consumer/views.py
+++ src/pbs_uua_consumer/views.py
@@ -84,14 +84,16 @@ def render_openid_request(request, openid_request, return_to, trust_root=None):
         return HttpResponse(form_html, content_type='text/html;charset=UTF-8')
 
 
-def render_failure(request, message, status=403,template_name='openid/failure.html', redirect_to=None):
+def render_response(request, message=None, status=403, template_name='openid/response.html', redirect_to=None):
     """Render an error page to the user."""
-    return render_to_response(template_name, {
-                    'redirect_to': redirect_to,
-                    'message': message,
-                    'popup': settings.OPENID_USE_POPUP_MODE,
-                    'sso_js_url': settings.OPENID_SSO_SERVER_JS_URL,
-                    }, context_instance=RequestContext(request))
+    response = render_to_string(
+        template_name, {
+            'redirect_to': redirect_to,
+            'message': message,
+            'popup': settings.OPENID_USE_POPUP_MODE,
+            'sso_js_url': settings.OPENID_SSO_SERVER_JS_URL,
+        }, context_instance=RequestContext(request))
+    return HttpResponse(response, status=status)
 
 def parse_openid_response(request):
     """Parse an OpenID response from a Django request."""
@@ -133,7 +135,7 @@ def login_begin(request, template_name='openid/login.html',
     try:
         openid_request = consumer.begin(openid_url)
     except DiscoveryFailure, exc:
-        return render_failure(
+        return render_response(
             request, "OpenID discovery error: %s" % (str(exc),), status=500)
 
     openid_request.addExtension(SignatureVerification(
@@ -167,26 +169,25 @@ def login_complete(request, redirect_field_name=REDIRECT_FIELD_NAME):
 
     openid_response = parse_openid_response(request)
     if not openid_response:
-        return render_failure(
+        return render_response(
             request, 'This is an OpenID relying party endpoint.')
 
-    print "****************************\n%s\n**********************" % openid_response.status
     if openid_response.status == SUCCESS:
         user = authenticate(openid_response=openid_response)
         if user is not None:
             if user.is_active:
                 auth_login(request, user)
-                return render_failure(request,'',redirect_to=sanitise_redirect_url(redirect_to))
+                return render_response(request, status=200, redirect_to=sanitise_redirect_url(redirect_to))
             else:
-                return render_failure(request, 'Disabled account')
+                return render_response(request, 'Disabled account')
         else:
-            return render_failure(request, 'Unknown user')
+            return render_response(request, 'Unknown user')
     elif openid_response.status == FAILURE:
-        return render_failure(
+        return render_response(
             request, 'OpenID authentication failed: %s' %
             openid_response.message)
     elif openid_response.status == CANCEL:
-        return render_failure(request, 'Authentication cancelled')
+        return render_response(request, 'Authentication cancelled')
     else:
         assert False, (
             "Unknown OpenID response type: %r" % openid_response.status)
