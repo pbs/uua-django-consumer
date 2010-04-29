1760404d757d993c6650ed427124a164cd9b9161
Ciprian Dimofte <ciprian.dimofte@lpro.leverpointinc.com>
2010-04-29 14:16:01 +0300

    Added authentication unittests

--- src/pbs_uua_consumer/tests.py
+++ src/pbs_uua_consumer/tests.py
@@ -3,13 +3,15 @@ from django.test.client import Client
 from django.http import Http404
 from pbs_uua_consumer import models
 from django.contrib.auth.models import User, Group
+from openid.consumer.consumer import SUCCESS
+from django.conf import settings
 
 class GenericTestCase(TestCase):
     fixtures = ['test_taxonomy.json',
                 'test_stations.json',
                 'test_programs.json',
                 'test_users.json',
-                'test_webobjects.json']
+                'test_users_openid.json',]
     
     def setUp(self):
         # Every test needs a client.
@@ -37,7 +39,28 @@ class GenericTestCase(TestCase):
         user = self.backend.get_user(2)
         self.failUnlessEqual(user.username, "admin")
     
-    
+    def test_authenticate_fail(self, openid_response = None):
+        try:
+            user = self.backend.authenticate()
+            self.failUnlessEqual(user, None)
+        except:
+            pass
+        
+        
+    #Test for OK authentication without:
+        #- creating from sreg
+        #- updating from sreg
+    #Those two will be tested individually
+    def make_mock():
+        class Mock(object): pass
+        x = Mock()
+        x.status = SUCCESS
+        x.identity_url = "http://192.168.1.121:8081/u/BdwAEHKnlO_W3zOWh0TMcQ"
+        return x
+
+    def test_authenticate_ok(self, openid_response = make_mock()):
+        user = self.backend.authenticate()
+        
 
     
     
