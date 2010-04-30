1c0a8cd410d779e62a3251a92b843d1dcbeec889
Ciprian Dimofte <ciprian.dimofte@lpro.leverpointinc.com>
2010-04-30 11:38:57 +0300

    Updated unittests for uus consumer backend

--- src/pbs_uua_consumer/tests.py
+++ src/pbs_uua_consumer/tests.py
@@ -5,6 +5,9 @@ from pbs_uua_consumer import models
 from django.contrib.auth.models import User, Group
 from openid.consumer.consumer import SUCCESS
 from django.conf import settings
+from openid import message
+
+
 
 class GenericTestCase(TestCase):
     fixtures = ['test_taxonomy.json',
@@ -12,6 +15,19 @@ class GenericTestCase(TestCase):
                 'test_programs.json',
                 'test_users.json',
                 'test_users_openid.json',]
+    
+    def make_mock(self):
+        class Mock(object):
+            def __init__(self):
+                pass
+            def getSignedNS(self):
+                return "http://openid.net/extensions/sreg/1.1"
+                
+        x = Mock()
+        x.message = message.Message()
+        x.status = SUCCESS
+        x.identity_url = "http://192.168.1.121:8081/u/BdwAEHKnlO_W3zOWh0TMcQ"
+        return x
     
     def setUp(self):
         # Every test needs a client.
@@ -31,6 +47,9 @@ class GenericTestCase(TestCase):
         from pbs_uua_consumer.auth import OpenIDBackend
         self.backend = OpenIDBackend()
         
+        self.mock = self.make_mock();
+        
+        
     
     def test_get_user(self):
         user = self.backend.get_user(-1)
@@ -39,28 +58,22 @@ class GenericTestCase(TestCase):
         user = self.backend.get_user(2)
         self.failUnlessEqual(user.username, "admin")
     
-    def test_authenticate_fail(self, openid_response = None):
+    def test_authenticate_fail(self):
         try:
-            user = self.backend.authenticate()
+            user = self.backend.authenticate(openid_response = None)
             self.failUnlessEqual(user, None)
         except:
             pass
         
         
-    #Test for OK authentication without:
-        #- creating from sreg
-        #- updating from sreg
-    #Those two will be tested individually
-    def make_mock():
-        class Mock(object): pass
-        x = Mock()
-        x.status = SUCCESS
-        x.identity_url = "http://192.168.1.121:8081/u/BdwAEHKnlO_W3zOWh0TMcQ"
-        return x
-
-    def test_authenticate_ok(self, openid_response = make_mock()):
-        user = self.backend.authenticate()
+    
+    def test_authenticate_ok(self):
+        openid_response = self.mock
+        #The make_mock is eroneous. Please help !
+        #user = self.backend.authenticate(openid_response = self.mock)
         
+
+
 
     
     
